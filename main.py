1ef2a7b9a7327184410f76c3b74d758511c8dd19
Zike01 <zikekomakoma@gmail.com>
2021-07-23 19:22:02 +0300

    Add flash messages on login route

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import login_required, logout_user, current_user, login_user, LoginManager, UserMixin
 
-# Meta Variables
+
+# META VARIABLES
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizzer.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -14,7 +15,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 year = datetime.date.today().year
 
-# Database Tables
+
+# DATABASE TABLES
 class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=False, nullable=False)
@@ -23,55 +25,65 @@ class Quiz(db.Model):
     total_questions = db.Column(db.Integer, unique=False, nullable=False)
     questions = db.Column(db.PickleType, unique=False, nullable=False)
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100))
     password = db.Column(db.String(100))
 
+
 db.create_all()
 
-# Authentication
+
+# AUTHENTICATION
 @login_manager.user_loader
 def load_user(user_id):
     if user_id is not None:
         return User.query.get(user_id)
     return None
 
+
 @login_manager.unauthorized_handler
 def unauthorized():
     return render_template("unautharized.html", year=year)
 
-# Routes
+
+# ROUTES
 @app.route("/")
 def home():
     is_logged_in = current_user.is_authenticated
     return render_template("index.html", year=year, logged_in=is_logged_in)
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
     if current_user.is_authenticated:
         return redirect(url_for("dashboard"))
+
     if request.method == "GET":
         return render_template("login.html", year=year)
     else:
         username = request.form["username"]
         password = request.form["password"]
         user = User.query.filter_by(username=username).first()
+        
+        # Check if user exists and credentials are valid
         if user:
-            real_password = user.password
-            if check_password_hash(real_password, password):
+            if check_password_hash(user.password, password):
                 login_user(user)
                 return redirect(url_for("dashboard"))
-            else:
-                return render_template("login.html", error="Invalid Username or Password", year=year)
-        else:
-            return render_template("login.html", error="Invalid Username or Password", year=year)
+        
+        # Redirect to login_page if user does not exist or credentials are invalid
+        flash("Invalid Username or Password", "danger")
+        return redirect(url_for('login_page'))
+
 
 @app.route("/browse")
 def browse():
     quizes = Quiz.query.all()
     is_logged_in = current_user.is_authenticated
     return render_template("browse.html", year=year, quizes=quizes, logged_in=is_logged_in)
+
 
 @app.route("/create-account", methods=["GET", "POST"])
 def create_account():
@@ -94,11 +106,13 @@ def create_account():
     
     return render_template("create-account.html", year=year)
 
+
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
 
 @app.route("/dashboard")
 @login_required
@@ -107,6 +121,7 @@ def dashboard():
     username = user.username
     own_quizes = Quiz.query.filter_by(author=username).all()
     return render_template("dashboard.html", year=year, is_dashboard=True, logged_in=True, user=user, own_quizes=own_quizes)
+
 
 @app.route("/create", methods=["GET", "POST"])
 @login_required
@@ -131,6 +146,7 @@ def create():
         # db.session.commit()
         # return render_template("success.html", year=year, logged_in=True)
 
+
 @app.route("/create-questions", methods=["GET", "POST"])
 @login_required
 def create_questions():
@@ -154,11 +170,13 @@ def start_quiz(id):
     else:
         return render_template("notfound.html", year=year, logged_in=is_logged_in), 404
 
+
 # Error Handling
 @app.errorhandler(404)
 def not_found(e):
     is_logged_in = current_user.is_authenticated
     return render_template("notfound.html", year=year, logged_in=is_logged_in), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
